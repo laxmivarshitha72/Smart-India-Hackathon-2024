6827d9521147b6debd1c12f9d7cbea30476ef5e2
laxmivarshitha72 <146968709+laxmivarshitha72@users.noreply.github.com>
2024-09-04 22:56:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     // Any additional JS functionality can be added here
+// });
+
+// Redirect to home.html after 5 seconds
+setTimeout(() => {
+    window.location.href = 'home.html';
+}, 3000);
+
